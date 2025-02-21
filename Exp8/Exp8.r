3a2b8a94edea0e63955f4d335e94d64aca042459
prasanth-192324099 <guruprasanthbabud4099.sse@saveetha.com>
2025-02-21 12:09:03 +0530

    Delete Exp8 directory

--- Exp8/Exp8.r
+++
@@ -1,7 +0,0 @@
-
-x <- c(4, 1, 5, 7, 10, 2, 50, 25, 90, 36)  
-y <- c(12, 5, 13, 19, 31, 7, 153, 72, 275, 110)  
-plot(x, y, main="Scatter Plot: Mobile Phones Sold vs Money Earned",
-     xlab="Mobile Phones Sold", ylab="Money Earned", 
-     col="blue", pch=16) 
-grid()  
